9e624fc70a71b33b1be343221e46d2f557cbcb07
Wagner Macedo <wagnerluis1982@gmail.com>
2016-07-15 11:03:28 -0300

    Processing messages with tags
    For now, the digester lacks the ability of process replies to these
    messages.

--- hashdigestbot/digester.py
+++ hashdigestbot/digester.py
@@ -20,6 +20,10 @@ class HashMessage:
 
 
 class Digester:
+    def __init__(self):
+        # Dict[str, Tuple[Set[str], List[HashMessage]]]
+        self._subjects = {}
+
     def feed(self, message: HashMessage) -> bool:
         """Give a message to process tags
 
@@ -29,8 +33,23 @@ class Digester:
         Returns:
             bool: Indicate if the message was added to the digest
         """
-        pass
+        tags = hashtags(message.text)
+        for tag in tags:
+            key = tag.lower()
+            if key not in self._subjects:
+                self._subjects[key] = ({tag}, [message])
+            else:
+                subject = self._subjects[key]
+                subject[0].add(tag)
+                subject[1].append(message)
 
-    def get_subject(self, tag: str) -> Sequence[HashMessage]:
+        return bool(tags)
+
+    def get_messages(self, tag: str) -> Sequence[HashMessage]:
         """Sequence of messages related to this tag"""
-        pass
+        key = tag.lower()
+        subject = self._subjects.get(key)
+        if subject:
+            return tuple(subject[1])
+
+        return ()
--- tests/test_digester.py
+++ tests/test_digester.py
@@ -1,9 +1,27 @@
 import unittest
 
-from hashdigestbot.digester import hashtags
+from hashdigestbot.digester import hashtags, HashMessage, Digester
 
 
 class TestDigester(unittest.TestCase):
+    def test_feed(self):
+        messages = (
+            # These messages should be fed?
+            HashMessage(1938, "Did you see #Superman?"),     # yes: message with hashtag
+            HashMessage(1939, "I am an useless message"),    # no: without HT or a reply
+            HashMessage(1940, "Yes, I saw", reply_to=1938),  # yes: replying a message with HT
+        )
+
+        digester = Digester()
+        self.assertTrue(digester.feed(messages[0]))
+        self.assertEqual(digester.get_messages("superman"), messages[0:1])
+
+        self.assertFalse(digester.feed(messages[1]))
+        self.assertEqual(digester.get_messages("superman"), messages[0:1])
+
+        self.assertTrue(digester.feed(messages[2]))
+        self.assertEqual(digester.get_messages("superman"), [messages[0], messages[2]])
+
     def test_hashtags(self):
         # one tag
         self.assertEqual(hashtags("I #love GDG"), {"love"})
