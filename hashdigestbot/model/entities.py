1bec2ce976b7d2c40d8fa21ff09ade60f8b9dd50
Wagner Macedo <wagnerluis1982@gmail.com>
2016-07-24 19:39:05 -0300

    Removed useless custom constructors of entities

--- hashdigestbot/model/entities.py
+++ hashdigestbot/model/entities.py
@@ -30,11 +30,6 @@ class HashTag(Base):
 class HashUser(Base):
     __tablename__ = 'users'
 
-    def __init__(self, id, friendly_name, username):
-        self.id = id
-        self.friendly_name = friendly_name
-        self.username = username
-
     id = PrimaryKey(Integer)
     friendly_name = Required(String)
     username = Required(String)
@@ -45,12 +40,6 @@ class HashUser(Base):
 
 class HashMessage(Base):
     __tablename__ = 'messages'
-
-    def __init__(self, id: int, text: str, chat_id: int, reply_to: int = None):
-        self.id = id
-        self.text = text
-        self.chat_id = chat_id
-        self.reply_to = reply_to
 
     id = PrimaryKey(Integer)
     text = Required(String)
