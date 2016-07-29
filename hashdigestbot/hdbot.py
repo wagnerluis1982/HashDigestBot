6baafd71747e30d7e2da94596dc68e515674c61f
Wagner Macedo <wagnerluis1982@gmail.com>
2016-07-29 08:57:41 -0300

    Moved command-line interface to `hashdigestbot.cli`

--- hashdigestbot/hdbot.py
+++ hashdigestbot/hdbot.py
@@ -1,8 +1,5 @@
-#!/usr/bin/env python3
-
 import logging
 
-import click
 from telegram.error import TelegramError
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -66,33 +63,3 @@ class HDBot:
 
     def stop(self):
         self.updater.stop()
-
-
-CONTEXT_SETTINGS = dict(
-    auto_envvar_prefix='HDBOT',
-)
-
-
-@click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('-t', '--token', required=True, help='Telegram bot token')
-@click.option('-u', '--db-url', required=True, help='Database url')
-@click.option('-c', '--chat', 'chats', required=True, multiple=True, metavar='NAME',
-              help='Allow digesting messages of a desired Telegram group in the format @groupname')
-def main(token, db_url, chats):
-    """Hashtag Digester Bot
-
-    A Telegram bot to make digests of tagged messages
-    """
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
-    LOG.setLevel(logging.DEBUG)
-    try:
-        digestbot = HDBot(token, db_url, chats)
-    except Exception as e:
-        raise click.UsageError(e)
-    else:
-        digestbot.start()
-
-
-if __name__ == "__main__":
-    main()
